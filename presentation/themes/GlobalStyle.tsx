6a0462b2d805f61e87611f04266af38e6f67b0be
남송녕 <62981041+Songnyeong-Nam@users.noreply.github.com>
2021-12-09 21:41:28 +0900

    GlobalStyle

---
+++ presentation/themes/GlobalStyle.tsx
@@ -0,0 +1,60 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Component = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
+@font-face{
+    font-family: 'yg-jalnan';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_four@1.2/JalnanOTF00.woff') format('woff'); 
+    font-weight: normal;
+    font-style: normal;
+}
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+body{
+    font-family: 'MalgunGothic','Nanum Gothic', sans-serif;
+    line-height: 1;
+}
+
+`;
+export default Component;
---
+++ presentation/themes/colors.ts
@@ -0,0 +1,11 @@
+const Colors = {
+  primary: {
+    main: `#F7323F`,
+  },
+  secondary: {
+    basic: `#222720`,
+    medium: `#99999a`,
+  },
+};
+
+export default Colors;
